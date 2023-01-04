4bd98fd014facd3521ce1c8945ef65c4502241c5
santu <101068062+54ntu@users.noreply.github.com>
2023-01-04 19:18:51 +0530

    Update fact.py

--- fact.py
+++ fact.py
@@ -12,3 +12,17 @@ else:
   for i in range(1,n1+1):
     factorial= factorial*i
   print("the factorial of ", n1, " is : " ,factorial)
+  
+  
+ 
+  
+#python program to find the factorial of a number using recursion function
+  
+def recursive_func(n):
+  if(n==1 or n==0):
+    return 1
+  else:
+    return n*recursive_func(n-1)
+
+number=int(input('enter the number of which you want to find the factorial :')) 
+recursive_func(number)
